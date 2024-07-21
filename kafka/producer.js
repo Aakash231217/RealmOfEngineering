6e55e790a2393007f57974ff01b03bd547f271fe
aakash <rktaakash@gmail.com>
2024-07-21 19:07:07 +0530

    cloudinary

--- kafka/producer.js
+++ kafka/producer.js
@@ -1,22 +1,36 @@
-const {kafka} = require('./client');
+const { kafka } = require("./client");
+const readline = require("readline");
 
-async function init(){
-     const producer = kafka.producer();
-     console.log('Connecting producer');
-     await producer.connect();
-     console.log("producer connected successful");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-     await producer.send({
-        topic:'rider-updates',
-        messages:[
-            {   
-                partition:0,
-                key:'location-update',
-                value:JSON.stringify({name:'Tony Stark',loc:"South"}),
-            },
-        ],
-     });
-   await producer.disconnect();
+async function init() {
+  const producer = kafka.producer();
+
+  console.log("Connecting Producer");
+  await producer.connect();
+  console.log("Producer Connected Successfully");
+
+  rl.setPrompt("> ");
+  rl.prompt();
+
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
+    await producer.send({
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name: riderName, location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    await producer.disconnect();
+  });
 }
 
 init();
